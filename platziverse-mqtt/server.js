c96dc6191daa6859592a8b50a1edd3d74f3dd0a8
Luis Rangel Castro <luisrangelc@rpaipos.com>
2018-08-25 14:03:12 -0600

    mqtt escuchar

--- platziverse-mqtt/server.js
+++ platziverse-mqtt/server.js
@@ -17,7 +17,47 @@ const settings = {
 }
 
 const server = new mosca.Server(settings)
+var cantidad = 0
+
+function handleFatalError(err) {
+    console.error(`${chalk.red('[fatal error]')} ${err.message}`)
+    console.error(err.stack)
+    process.exit(1)
+}
 
 server.on('ready', () => {
     console.log(`${chalk.green('[platziverse-mqtt]')} server is running`)
+    console.log(`Cantidad ${cantidad}`)
 })
+
+server.on('clientConnected', client => {
+    cantidad = 0
+    console.log('----------------------->')
+    let start = new Date().toUTCString();
+    console.log(start)
+    console.time("concatenation");
+    debug(`Client Connected: ${client.id}`)
+    console.log(`clientConnected Cantidad ${cantidad}`)
+    console.log('')
+})
+
+server.on('clientDisconnected', client => {
+    cantidad++
+    debug(`Client Disconnected: ${client.id}`)
+    console.timeEnd("concatenation");
+    console.log('<-----------------------')
+    console.log(`clientDisconnected Cantidad ${cantidad}`)
+    console.log('')
+})
+
+server.on('published', (packet, client) => {
+    cantidad++
+    debug(`Received: ${packet.topic}`)
+    debug(`Payload: ${packet.payload}`)
+    console.log(`published Cantidad ${cantidad}`)
+    console.log('')
+})
+
+server.on('error', handleFatalError)
+server.on('uncaughtException', handleFatalError)
+server.on('unhandledRejection', handleFatalError)
